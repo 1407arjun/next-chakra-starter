15a66b7702e690ab6e1ab79dc9dea237dec222f9
Arjun Sivaraman <arjun140702@gmail.com>
2023-06-12 18:52:52 +0530

    Format all files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider theme={theme}><Component {...pageProps} /></ChakraProvider>
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
